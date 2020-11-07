ba0111f2e89da436d03d11c4a6f7f72292bafa17
modisujeet <sujeetraj.121@gmail.com>
2020-11-07 17:45:41 +0530

    adding form and validation function on it

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -8,6 +8,10 @@ class FormController extends Controller
 {
     //
     function getData(Request $req){
+        $req -> validate([
+            'username' => 'required | max : 30 ',
+            'userpassword' => 'required | min : 6 '
+        ]);
         return $req -> input();
     }
 }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,7 +1,34 @@
 <h1>User Login</h1>
+
+<!-- This is for showing error if any of the field 
+user forgot to input and try to submit it will throw 
+an error msg above the form.
+
+@if($errors -> any())
+    @foreach ($errors -> all() as $err)
+    <h5>{{$err}}</h5>
+    @endforeach
+ @endif  -->
+
 <form action="form" method="POST">
   @csrf
-  <input type="text" name="username" placeholder="Enter User Id"> <br> <br>
-  <input type="password" name="userpassword" placeholder="enter user password"> <br><br>
+  <input type="text" name="username" 
+    placeholder="Enter User Id"> <br> 
+    <span style = "color: red">
+      @error('username') 
+         {{$message}}
+      @enderror 
+    </span>
+    <br>
+  
+  <input type="password" name="userpassword" 
+    placeholder="enter user password"> <br>
+    <span style = "color: red">
+        @error('userpassword') 
+          {{$message}}
+        @enderror 
+      </span>
+    <br>
+  
   <button type="submit"> Login </button>
 </form>
